0eea2340742dd53839f7da7a3c6363e75febc94d
Harvey <wickwickthedog@Harveys-MacBook-Air.local>
2023-03-12 14:52:52 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `WickWickTheDog`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
